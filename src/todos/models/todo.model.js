93c7c9dcd6213d4884904375604a55754cfab40c
carlosgudin <carlosandresgudin@gmail.com>
2024-03-21 22:21:38 -0500

    todo-app

--- src/todos/models/todo.model.js
+++ src/todos/models/todo.model.js
@@ -1,9 +1,11 @@
+import {v4 as uuid} from 'uuid';
+
 class Todo{
     /**
      * @param {String} description
      */
     constructor( description ){
-        this.id = 1;
+        this.id = uuid();
         this.description = description;
         this.done = false;
         this.createAt = new Date();
